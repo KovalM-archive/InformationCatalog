779f88e2a50d70904fe0b513a15edc18ec114a56
KovalM <m.koval@mail.by>
2016-03-08 16:39:53 +0300

    first servlet

---
+++ CatalogServer/src/kmv/servlet/FirstServlet.java
@@ -0,0 +1,15 @@
+package kmv.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends HttpServlet{
+    public void doGet(HttpServletRequest pRequest, HttpServletResponse pResponse) throws ServletException, IOException {
+        PrintWriter printWriter = pResponse.getWriter();
+        printWriter.println(pRequest.getRequestURI());
+    }
+}
